d8792d90161bf36c0450cd8cf59d038adbce274c
Julián David Sánchez Gallego <julian.sanchez.kommit@gmail.com>
2021-10-16 12:28:22 -0500

    Update repo with the chapter twenty three files

---
+++ chapter_twenty_three/conclusion.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# The course has been concluded. For more information, please refer
+# to the official ruby documentation (https://www.ruby-lang.org/es/documentation/)
+# and websites such as Stack Overflow (https://es.stackoverflow.com/).
